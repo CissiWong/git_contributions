17b18b63e1766b8f37a8ba3f3f8122e4d56220ee
Cecilia Wong <cissi.wong@gmail.com>
2019-05-05 18:34:25 +0200

    added env. variable and oauth - query successful

--- assessment/src/index.js
+++ assessment/src/index.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient, { InMemoryCache } from 'apollo-boost';
+import { gql } from 'apollo-boost';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+
+const client = new ApolloClient({
+    uri: "https://api.github.com/graphql",
+    headers: {
+        authorization: `Bearer ${process.env.REACT_APP_GIT_TOKEN}`,
+    },
+    cache: new InMemoryCache(),
+});
+
+client
+  .query({
+    query: gql`
+    {
+        __type:user(login: "CissiWong") {
+          name
+        }
+      }`
+  })
+  .then(result => console.log(result));
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
